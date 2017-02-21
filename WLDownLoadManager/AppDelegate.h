c0d2aa8e2490ee76566cabf060c92aec0d480c8b
threenoodles <threenoodles@163.com>
2017-02-21 11:37:47 +0800

    [init]

---
+++ WLDownLoadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WLDownLoadManager
+//
+//  Created by Mac on 17/2/21.
+//  Copyright © 2017年 wwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WLDownLoadManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WLDownLoadManager
+//
+//  Created by Mac on 17/2/21.
+//  Copyright © 2017年 wwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
